deffc9c0f0b8fb10686085fcc66f20601508a77b
AmineSen0AT0 <amine.zerarga@se.univ-bejaia.dz>
2024-03-03 14:34:52 +0100

    some modification

--- Car_Counter&Speed_OpVino.py
+++ Car_Counter&Speed_OpVino.py
@@ -8,7 +8,7 @@ import time
 from sort import *
 from PIL import Image, ImageEnhance, ImageFilter
 #
-#### enhacing video frames ########
+#### enhacing video frames ##############
 def apply_pil_enhancements(frame):
     pil_image = Image.fromarray(frame)  # Convert OpenCV frame to PIL image
 
